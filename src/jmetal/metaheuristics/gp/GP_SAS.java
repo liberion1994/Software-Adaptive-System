1cb8b0a3349167dc13004c38b7e9fee766173fcd
keli.genius <keli.genius@gmail.com>
2016-04-18 22:26:21 +0100

    GP codes for SAS

---
+++ src/jmetal/metaheuristics/gp/GP_SAS.java
@@ -0,0 +1,230 @@
+//  NSGAII.java
+//
+//  Author:
+//       Antonio J. Nebro <antonio@lcc.uma.es>
+//       Juan J. Durillo <durillo@lcc.uma.es>
+//
+//  Copyright (c) 2011 Antonio J. Nebro, Juan J. Durillo
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jmetal.metaheuristics.gp;
+
+import jmetal.core.*;
+import jmetal.problems.SASSolutionInstantiator;
+import jmetal.util.comparators.CrowdingComparator;
+import jmetal.util.*;
+
+/**
+ * 
+ * @author keli, taochen
+ *
+ */
+public class GP_SAS extends Algorithm {
+
+	private SASSolutionInstantiator factory = null;
+
+	/**
+	 * Constructor
+	 * @param problem Problem to solve
+	 */
+	public GP_SAS(Problem problem) {
+		super (problem) ;
+	} // NSGAII
+
+
+  	/**
+  	 * Constructor
+  	 * @param problem Problem to solve
+  	 */
+	public GP_SAS(Problem problem, SASSolutionInstantiator factory) {
+		super(problem);
+        this.factory = factory;
+	}
+
+	/**   
+	 * Runs the NSGA-II algorithm.
+	 * @return a <code>SolutionSet</code> that is a set of non dominated solutions
+	 * as a result of the algorithm execution
+	 * @throws JMException 
+	 */
+	public SolutionSet execute() throws JMException, ClassNotFoundException {
+		
+		if (factory == null) {
+			throw new RuntimeException("No instance of SASSolutionInstantiator found!");
+		}
+		
+		int type;
+		
+		int populationSize;
+		int maxEvaluations;
+		int evaluations;
+
+		int requiredEvaluations; // Use in the example of use of the
+		// indicators object (see below)
+
+		// knee point which might be used as the output
+		Solution kneeIndividual = factory.getSolution(problem_);
+
+		SolutionSet population;
+		SolutionSet offspringPopulation;
+		SolutionSet union;
+
+		Operator mutationOperator;
+		Operator crossoverOperator;
+		Operator selectionOperator;
+
+		Distance distance = new Distance();
+
+		//Read the parameters
+		populationSize = ((Integer) getInputParameter("populationSize")).intValue();
+		maxEvaluations = ((Integer) getInputParameter("maxEvaluations")).intValue();
+
+		//Initialize the variables
+		population = new SolutionSet(populationSize);
+		evaluations = 0;
+
+		requiredEvaluations = 0;
+
+		//Read the operators
+		mutationOperator  = operators_.get("mutation");
+		crossoverOperator = operators_.get("crossover");
+		selectionOperator = operators_.get("selection");
+
+		// Create the initial solutionSet
+		Solution newSolution;
+		for (int i = 0; i < populationSize; i++) {
+			newSolution = factory.getSolution(problem_);
+			problem_.evaluate(newSolution);
+			problem_.evaluateConstraints(newSolution);
+			fitnessAssignment(newSolution);	// assign fitness value to each solution
+			evaluations++;
+			population.add(newSolution);
+		} //for       
+
+		// Generations 
+		while (evaluations < maxEvaluations) {
+
+			// Create the offSpring solutionSet
+			offspringPopulation = new SolutionSet(populationSize);
+			Solution[] parents = new Solution[2];
+			for (int i = 0; i < (populationSize / 2); i++) {
+				if (evaluations < maxEvaluations) {
+					//obtain parents
+					parents[0] = (Solution) selectionOperator.execute(population);
+					parents[1] = (Solution) selectionOperator.execute(population);
+					Solution[] offSpring = (Solution[]) crossoverOperator.execute(parents);
+					mutationOperator.execute(offSpring[0]);
+					mutationOperator.execute(offSpring[1]);
+					problem_.evaluate(offSpring[0]);
+					problem_.evaluateConstraints(offSpring[0]);
+					problem_.evaluate(offSpring[1]);
+					problem_.evaluateConstraints(offSpring[1]);
+					fitnessAssignment(offSpring[0]);
+					fitnessAssignment(offSpring[1]);
+					offspringPopulation.add(offSpring[0]);
+					offspringPopulation.add(offSpring[1]);
+					evaluations += 2;
+				} // if                            
+			} // for
+
+			// Create the solutionSet union of solutionSet and offSpring
+			union = ((SolutionSet) population).union(offspringPopulation);
+			
+			// Environmental selection based on the fitness value of each solution 
+			int[] idxArray = new int[union.size()];
+			double[] pData = new double[union.size()];
+			for (int i = 0; i < union.size(); i++) {	// Is there a faster way?
+				idxArray[i] = i;
+				pData[i]    = union.get(i).getFitness();
+			}
+			Utils.QuickSort(pData, idxArray, 0, union.size() - 1); 
+			
+			population.clear();
+			for (int i = 0; i < populationSize; i++)
+				population.add(union.get(idxArray[i]));
+
+		} // while
+
+		// Return as output parameter the required evaluations
+		setOutputParameter("evaluations", requiredEvaluations);
+
+		// Return the first non-dominated front
+		Ranking ranking = new Ranking(population);
+		return ranking.getSubfront(0);
+	} // execute
+	
+	/**
+	 * This is used to assign fitness value to a solution, according to weighted sum strategy.
+	 * @param cur_solution
+	 */
+	public void fitnessAssignment(Solution cur_solution) {
+		double cur_fitness = 0.0;
+		for (int i = 0; i < problem_.getNumberOfObjectives(); i++) {
+			cur_fitness += 1.0 * cur_solution.getObjective(i); 
+		}
+		cur_solution.setFitness(cur_fitness);
+	}
+	
+	/**
+	 * This is used to find the knee point from a set of solutions
+	 * 
+	 * @param population
+	 * @return
+	 */
+//	public Solution kneeSelection(SolutionSet population_) {		
+//		int[] max_idx    = new int[problem_.getNumberOfObjectives()];
+//		double[] max_obj = new double[problem_.getNumberOfObjectives()];
+//		int populationSize_ = population_.size();
+//		// finding the extreme solution for f1
+//		for (int i = 0; i < populationSize_; i++) {
+//			for (int j = 0; j < problem_.getNumberOfObjectives(); j++) {
+//				// search the extreme solution for f1
+//				if (population_.get(i).getObjective(j) > max_obj[j]) {
+//					max_idx[j] = i;
+//					max_obj[j] = population_.get(i).getObjective(j);
+//				}
+//			}
+//		}
+//
+//		if (max_idx[0] == max_idx[1])
+//			System.out.println("Watch out! Two equal extreme solutions cannot happen!");
+//		
+//		int maxIdx;
+//		double maxDist;
+//		double temp1 = (population_.get(max_idx[1]).getObjective(0) - population_.get(max_idx[0]).getObjective(0)) * 
+//				(population_.get(max_idx[0]).getObjective(1) - population_.get(0).getObjective(1)) - 
+//				(population_.get(max_idx[0]).getObjective(0) - population_.get(0).getObjective(0)) * 
+//				(population_.get(max_idx[1]).getObjective(1) - population_.get(max_idx[0]).getObjective(1));
+//		double temp2 = Math.pow(population_.get(max_idx[1]).getObjective(0) - population_.get(max_idx[0]).getObjective(0), 2.0) + 
+//				Math.pow(population_.get(max_idx[1]).getObjective(1) - population_.get(max_idx[0]).getObjective(1), 2.0);
+//		double constant = Math.sqrt(temp2);
+//		double tempDist = Math.abs(temp1) / constant;
+//		maxIdx  = 0;
+//		maxDist = tempDist;
+//		for (int i = 1; i < populationSize_; i++) {
+//			temp1 = (population_.get(max_idx[1]).getObjective(0) - population_.get(max_idx[0]).getObjective(0)) *
+//					(population_.get(max_idx[0]).getObjective(1) - population_.get(i).getObjective(1)) - 
+//					(population_.get(max_idx[0]).getObjective(0) - population_.get(i).getObjective(0)) * 
+//					(population_.get(max_idx[1]).getObjective(1) - population_.get(max_idx[0]).getObjective(1));
+//			tempDist = Math.abs(temp1) / constant;
+//			if (tempDist > maxDist) {
+//				maxIdx  = i;
+//				maxDist = tempDist;
+//			}
+//		}
+//		
+//		return population_.get(maxIdx);
+//	}
+} // NSGA-II
---
+++ src/jmetal/operators/selection/BinaryTournamentSAS.java
@@ -0,0 +1,99 @@
+//  BinaryTournament2.java
+//
+//  Author:
+//       Antonio J. Nebro <antonio@lcc.uma.es>
+//       Juan J. Durillo <durillo@lcc.uma.es>
+//
+//  Copyright (c) 2011 Antonio J. Nebro, Juan J. Durillo
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jmetal.operators.selection;
+
+import java.util.Comparator;
+import java.util.HashMap;
+
+import jmetal.core.Operator;
+import jmetal.core.Solution;
+import jmetal.core.SolutionSet;
+import jmetal.util.JMException;
+import jmetal.util.comparators.DominanceComparator;
+import jmetal.util.PseudoRandom;
+
+/**
+ * This class implements an operator for binary selections using the same code
+ * in Deb's NSGA-II implementation
+ */
+public class BinaryTournamentSAS extends Selection {
+  
+  /**
+   * a_ stores a permutation of the solutions in the solutionSet used
+   */
+  private int a_[];
+  
+  /**
+   *  index_ stores the actual index for selection
+   */
+  private int index_ = 0;
+    
+  /**
+   * Constructor
+   * Creates a new instance of the Binary tournament operator (Deb's
+   * NSGA-II implementation version)
+   */
+  public BinaryTournamentSAS(HashMap<String, Object> parameters)
+  {
+  	super(parameters) ;
+  } // BinaryTournament2
+    
+  /**
+  * Performs the operation
+  * @param object Object representing a SolutionSet
+  * @return the selected solution
+  */
+  public Object execute(Object object)    
+  {
+    SolutionSet population = (SolutionSet)object;
+    if (index_ == 0) //Create the permutation
+    {
+      a_= (new jmetal.util.PermutationUtility()).intPermutation(population.size());
+    }
+ 
+    Solution solution1,solution2;
+    solution1 = population.get(a_[index_]);
+    solution2 = population.get(a_[index_+1]);
+        
+    index_ = (index_ + 2) % population.size();
+        
+    if (solution1.getFitness() < solution2.getFitness())
+    	return solution1;
+    else if (solution1.getFitness() > solution2.getFitness())
+    	return solution2;
+    else
+    	if (PseudoRandom.randDouble() < 0.5) 
+    		return solution1;
+    	else
+    		return solution2;
+  } // execute
+
+  @Override
+  public Object execute_5(Object object) throws JMException {
+    return null;
+  }
+
+  @Override
+  public Object execute_6(Object object) throws JMException {
+    return null;
+  }
+} // BinaryTournament_SAS
